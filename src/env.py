9ebf58dcbafa642f05b8d3583ab760908111dc9f
Seong-hun Kim <kshoon92@gmail.com>
2022-05-17 19:31:16 +0900

    Add a multicopter class

---
+++ src/env.py
@@ -0,0 +1,99 @@
+import fym
+import numpy as np
+from fym.utils.rot import hat
+
+
+def cross(x, y):
+    return np.cross(x, y, axis=0)
+
+
+class Multicopter(fym.BaseEnv):
+    """S. Mallavalli and A. Fekih, “A fault tolerant tracking control for a
+    quadrotor UAV subject to simultaneous actuator faults and exogenous
+    disturbances,” International Journal of Control, vol. 93, no. 3, pp.
+    655–668, Mar. 2020, doi: 10.1080/00207179.2018.1484173."""
+
+    """ Physical constants """
+    g = 9.81
+    """ Physical properties """
+    m = 1.00  # [kg] mass
+    r = 0.24  # [m] torque arm
+    _J = np.diag([8.1, 8.1, 14.2]) * 1e-3
+    _Jr = np.diag([0, 0, 1.04]) * 1e-6
+    b = 5.42e-5  # [N s^2 / rad^2] thrust coeff.
+    d = 1.1e-6  # [N m s^2 / rad^2] reaction torque coeff.
+    Kf = np.diag([5.567, 5.567, 6.354]) * 1e-4  # [N.s/m] drag coeff.
+    Kt = np.diag([5.567, 5.567, 6.354]) * 1e-4  # [N.s/rad] aerodynamic drag
+    rotorf_min = 0  # [N]
+    # rotorf_max = b * 523.0**2  # [N]
+    rotorf_max = 20  # [N]
+    """ Auxiliary constants """
+    e3 = np.vstack((0, 0, 1))
+    nrotors = 4
+    """ Configuration
+          ^ x
+          |
+         (0)
+    - (3) + (1) -> y
+         (2)
+          |
+    """
+    # (F, M) = B @ rotorfs
+    # rotorfs = b * rotorws**2; rotorws: rotor speed
+    B = np.array(
+        [
+            [1, 1, 1, 1],
+            [0, -r, 0, r],
+            [r, 0, -r, 0],
+            [-d / b, d / b, -d / b, d / b],
+        ]
+    )
+
+    def __init__(self):
+        super().__init__()
+        self.pos = fym.BaseSystem(np.vstack((0, 0, -2)))
+        self.vel = fym.BaseSystem(np.zeros((3, 1)))
+        self.R = fym.BaseSystem(np.eye(3))
+        self.omega = fym.BaseSystem(np.zeros((3, 1)))
+        self.J = self._J
+
+    def deriv(self, pos, vel, R, omega, rotorfs):
+        u = self.B @ rotorfs
+        fT, M = u[:1], u[1:]
+
+        dpos = vel
+        dvel = (
+            1
+            / self.m
+            * (+self.m * self.g * self.e3 + R @ (-fT * self.e3) - self.Kf @ vel)
+        )
+        dR = R @ hat(omega)
+        domega = self.Jinv @ (
+            M - cross(omega, self.J @ omega) - np.linalg.norm(omega) * self.Kt @ omega
+        )
+        return dpos, dvel, dR, domega
+
+    def set_dot(self, t, rotorfs_cmd):
+        states = self.observe_list()
+        rotorfs = self.set_valid(t, rotorfs_cmd)
+        dots = self.deriv(*states, rotorfs)
+        self.pos.dot, self.vel.dot, self.R.dot, self.omega.dot = dots
+        return dict(rotorfs=rotorfs)
+
+    def set_valid(self, t, rotorfs_cmd):
+        """Saturate the rotor angular velocities and set faults."""
+        rotorfs = np.clip(rotorfs_cmd, self.rotorf_min, self.rotorf_max)
+        return self.get_Lambda(t) @ rotorfs
+
+    def get_Lambda(self, t):
+        Lambda = self.Lambda  # Currently, the fault occurs at the start
+        return Lambda
+
+    @property
+    def J(self):
+        return self._J
+
+    @J.setter
+    def J(self, J):
+        self._J = J
+        self.Jinv = np.linalg.inv(J)
